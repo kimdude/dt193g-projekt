ac63b809440d1b8a41a7abd26a652478039de944
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-01-07 14:38:52 +0100

    Lagt till anslutning till databasen och hapi server,

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { Client } = require('pg');
+const Hapi = require('@hapi/hapi');
+require('dotenv').config();
+
+//Connecting to server
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    });
+
+    //Connecting to database
+    require('./database/db');
+
+    //Requiring routes
+    require('./routes/product.routes');
+    require('./routes/admin.routes');
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+}
+
+process.on('unhandledRejection', (error) => {
+    console.log(error);
+    process.exit(1);
+});
+
+init();
